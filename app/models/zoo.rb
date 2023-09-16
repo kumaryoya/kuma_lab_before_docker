ab5c3709d10320d8a676d86195511df349eca6d4
Okuma Ryoya <kumaryoya05210521@yahoo.co.jp>
2023-09-16 10:40:25 +0900

    動物園テーブルを作成しました

---
+++ app/models/zoo.rb
@@ -0,0 +1,7 @@
+class Zoo < ApplicationRecord
+  validates :name, presence: true, uniqueness: true
+  validates :location, presence: true, uniqueness: true
+  validates :area, presence: true
+  validates :prefecture, presence: true
+  validates :link, presence: true, uniqueness: true
+end

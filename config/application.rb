d4bb4a8e6ec139aca88f8f66794f3f4f453d9df7
RyoyaOkuma <kumaryoya05210521@gmail.com>
2023-09-29 00:25:49 +0900

    Revert "renderのセットアップ"

--- config/application.rb
+++ config/application.rb
@@ -22,6 +22,5 @@ module KumaLab
     #
     # config.time_zone = 'Central Time (US & Canada)'
     # config.eager_load_paths << Rails.root.join('extras')
-    config.hosts << 'xxx.onrender.com'
   end
 end

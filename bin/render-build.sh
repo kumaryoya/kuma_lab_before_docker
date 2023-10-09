397558eb1d36a0adb27309c9924a754152bb47f1
Okuma Ryoya <kumaryoya05210521@yahoo.co.jp>
2023-10-09 20:57:58 +0900

    ビューを修正しました

--- bin/render-build.sh
+++ bin/render-build.sh
@@ -4,6 +4,7 @@ set -o errexit
 
 bundle install
 rails tmp:cache:clear
+bundle exec rake assets:clobber
 bundle exec rake assets:clean
 bundle exec rake assets:precompile
 bundle exec rake db:migrate

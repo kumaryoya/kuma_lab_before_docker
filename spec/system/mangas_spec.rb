3702e2d15d19abb901cb4d3eae7df0529ab46a09
Okuma Ryoya <kumaryoya05210521@yahoo.co.jp>
2023-10-16 13:27:58 +0900

    システムスペックを書きました

--- spec/system/mangas_spec.rb
+++ spec/system/mangas_spec.rb
@@ -16,14 +16,14 @@ describe 'mangas' do
 
     it 'with ジャンルを選択することができる' do
       click_link 'アクション'
-      sleep(40)
+      sleep(45)
       expect(page).to have_content('キングダム')
       expect(page).to have_current_path mangas_path, ignore_query: true
     end
 
     it 'with Twitterで共有することができる' do
       click_link 'アクション'
-      sleep(40)
+      sleep(45)
       new_window = window_opened_by { click_link 'Twitterで共有する' }
       sleep(5)
       within_window new_window do
@@ -34,7 +34,7 @@ describe 'mangas' do
 
     it 'with 最初からやり直すことができる' do
       click_link 'アクション'
-      sleep(40)
+      sleep(45)
       click_link '最初からやり直す'
       expect(page).to have_content('読みたい漫画のジャンルを選択してください。')
       expect(page).to have_current_path genre_mangas_path, ignore_query: true
